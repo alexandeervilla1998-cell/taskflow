5760dc1f9430935a201f8498c281928d40aacaec
alexandeervilla1998-cell <alexandeervilla1998@gmail.com>
2026-06-25 17:18:06 -0600

    feat: crear repositorio Usuario

---
+++ backend/src/main/java/com/taskflow/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.taskflow.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.taskflow.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByCorreo(String correo);
+
+    boolean existsByCorreo(String correo);
+}
